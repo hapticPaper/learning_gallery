903883ebb203191a766df882df7bb9ab0a80d624
Ian Rubenstein <ian@hapticpaper.com>
2025-12-16 05:05:17 -0500

    gtm tag id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const gtmId = process.env.NEXT_PUBLIC_GTM_ID ?? "G-VPHFT711T9";
+const gtmId = process.env.NEXT_PUBLIC_GTM_ID ?? "GTM-P43CMVDL";
 
 export const metadata: Metadata = {
   title: {
